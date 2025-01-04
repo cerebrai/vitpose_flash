287a2af89639f3a27a8c6266ce73e241fdbbbab7
Safwan Khalid <safwan@movrs.com>
2025-01-04 15:05:58 +0500

    First commit to init repo:
    The code is working and the testep file test1.py
    is used to run and find the timings. I will change
    test1.py later. The repo is ready to be push to
    remote.

---
+++ vitpose_flash/onepose/__init__.py
@@ -0,0 +1,2 @@
+from vitpose_flash.onepose.models import create_model, list_models
+from vitpose_flash.onepose.visualization import visualize_keypoints
---
+++ vitpose_flash/onepose/datasets/__init__.py
@@ -0,0 +1,2 @@
+import vitpose_flash.onepose.datasets.coco
+import vitpose_flash.onepose.datasets.mpii
